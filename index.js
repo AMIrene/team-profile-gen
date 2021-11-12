df54777d329b9ae2783a7a9d52456480e2b0ce84
AMIrene <f.adammanik@gmail.com>
2021-11-12 23:27:26 +1100

    addemployee and addnewemployee function

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const Manager = require('./lib/Manager');
 
 const staffGroup = [];
 
+
 const addEmployee = () => {
     return inquirer.prompt([
         {
@@ -89,6 +90,82 @@ const addEmployee = () => {
         })
 };
 
+const addAnotherEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What is your role?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern']
+            
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+
+        },
+        {
+            type: 'input',
+            message: 'What is your employee ID?',
+            name: 'id',
+
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+
+        },
+        {
+            type: 'input',
+            message: 'What is your contact number?',
+            name: 'officeNumber',
+            when: (answer) => answer.role === "Manager",
+
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'github',
+            when: (answer) => answer.role === "Engineer",
+
+        },
+        {
+            type: 'input',
+            message: 'What is your school name?',
+            name: 'school',
+            when: (answer) => answer.role === "Intern",
+
+        },
+        
+        
+    ])
+    .then(newUserInput => {
+        let { name, id, role, email, officeNumber, github, school } = newUserInput;
+        let user;
+
+        if (role === 'Manager') {
+             
+            user = new Manager(name, id, email, officeNumber);
+            console.log(user);
+        }
+        else if (role === 'Engineer') {
+            user = new Engineer(name, id, email, github);
+        }
+
+        else if (role === 'Intern') {
+            user = new Intern(name, id, email, school);
+        }
+    
+        staffGroup.push(newUserInput);
+    })
+};
+    
+
+
 const writeFile = data => {
     fs.writeFile('./dist/index.html', data, err => {
         // if there is an error 
@@ -103,7 +180,7 @@ const writeFile = data => {
 }; 
 
 addEmployee()
-  
+  .then (addAnotherEmployee)
   .then(staffGroup => {
     return renderHTML(staffGroup);
   })
